ed905bbd379241ed268730c5c1b3fd9f3ae2fe89
craigy <craigy@users.noreply.github.com>
2016-06-30 07:03:27 -0400

    Add number of rolls option.

--- src/cljs/dice_simulator/core.cljs
+++ src/cljs/dice_simulator/core.cljs
@@ -7,11 +7,14 @@
 (defonce app-state
   (r/atom
     {:dice 2
+     :rolls 1
      :faces 2
      :iterations 10000
      :result nil}))
 
 (def dice [1 2 3 4 5 6])
+
+(def rolls [1 2 3 4 5 6])
 
 (def faces [1 2 3 4 5 6])
 
@@ -28,22 +31,32 @@
 
 (defn sim []
   (let [d (:dice @app-state)
+        r (:rolls @app-state)
         f (:faces @app-state)
         n (:iterations @app-state)]
     (loop [i 0 res {true 0 false 0}]
       (if (= i n)
         (swap! app-state assoc :result res)
-        (let [r (take d (repeatedly #(rand-int 6)))
-              k (reduce #(or %1 (< %2 f)) false r)]
+        (let [x (take (* r d) (repeatedly #(rand-int 6)))
+              k (reduce #(or %1 (< %2 f)) false x)]
           (recur (inc i) (update-in res [k] inc)))))))
 
 (def sep [:div {:style {:height "16px"}}])
+
+(defn round-2 [n d]
+  (/
+    (int
+      (* 10000 (/ n d)))
+    100))
 
 (defn app []
   [:div
    [:div
     [:div "Number of Dice"]
     (doall (map-indexed #(button :dice dice %) dice))]
+   [:div
+    [:div "Number of Rolls"]
+    (doall (map-indexed #(button :rolls rolls %) rolls))]
    [:div
     [:div "Goal faces"]
     (doall (map-indexed #(button :faces faces %) faces))]
@@ -56,13 +69,14 @@
      {:style {:font-size "34px"}
       :on-click sim
       :disabled (not (and (:dice @app-state)
+                          (:rolls @app-state)
                           (:faces @app-state)
                           (:iterations @app-state)))}
      "Go!"]]
    sep
    (when-let [res (:result @app-state)]
      [:div
-       [:div (str (/ (int (* 10000 (/ (res true) (+ (res true) (res false))))) 100) "%")]
+       [:div (str (round-2 (res true) (+ (res true) (res false))) "%")]
        [:div (str res)]])])
 
 (defn ^:export run[]
